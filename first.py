4470a698add68a8aa00caccc0f392f054c1022b0
naveedfatima <naveedfatimaumer@gmail.com>
2024-12-13 22:08:01 +0500

    practice.ipynb

---
+++ first.py
@@ -0,0 +1,17 @@
+print("hello world") 
+a=1
+print(type(a))
+b= True
+print(type(b))
+c="fatima"
+print(type(c))
+d=7.8
+print(type(d))
+e="7"
+f=int(e)
+print(type(f))
+g=int(input("enter your age no.1"))
+print("g: ", g)
+#h=int(input("enter your age no.2"))
+#print("h:", h)
+#print("sum=", g+h)
